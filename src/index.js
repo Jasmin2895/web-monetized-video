ed1ff29ad6c04b4e6316e69ec73f59e837bbd6e9
buckleader <jasmin28virdi@gmail.com>
2020-05-23 04:42:46 +0530

    html element class created

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
-const template = document.createElement('template');
+const getStyle = (width = '300', heigth = '150') => {
+    return `
+    .video-component {
+        width: ${width}px;
+        height: ${height}px;
+    }
+    `
+  }
 
-//support for both the video and iframe should be there.
 class WebMonetizedVideo extends HTMLElement {
     constructor()  {
         super()
-        this.hasWebMonetizationEnabled= false;
         // runs whenever an element is created, but before the element is attached to the document.
         // it will be used to create the initial state, event listeners and creating shaow DOM.
-        let template = document.getElementById("web-monetized-video");
+        this.hasWebMonetizationEnabled= false;
+        this.isWebMonetized();
+        // let template = document.getElementById("web-monetized-video"); // confirm template required or not.
+    }
 
+    isWebMonetized() {
+        if(!document.monetized) {
+            const h4 = document.createElement("h4");
+            h4.innerHTML = "Your browser is not web moentized"
+            document.body.appendChild(h4);
+        } else {
+            this.hasWebMonetizationEnabled = true;
+        }
+    }
 
+    enableWebMonetization() {
+        if(!this.hasWebMonetizationEnabled) {
+            const monetizationTag = document.createElement('meta');
+            monetizationTag.name = "monetization";
+            monetizationTag.content = "payment_pointer_comes_here";
+            document.head.appendChild(monetizationTag);
+        }
+    }
+
+    disableWebMonatization() {
+        this.hasWebMonetizationEnabled = false;
+        document.querySelector('meta[name="monetization"]').remove();
     }
 
     connectedCallback() {
         //called when the element is inserted in the DOM.
         console.log('connected!');
         this.createShadowRoot();
-        this.source = this.getAttribute('source');
+        this.url = this.getAttribute('url');
         this.width = this.getAttribute('width');
         this.heigth = this.getAttribute('heigth');
         this.render();                                                                           
@@ -25,51 +54,42 @@ class WebMonetizedVideo extends HTMLElement {
     render() {
         const video = document.createElement("video") // add video element define heigth and width and controls in html file
         video.setAttribute("id", "video");
-        video.classList.add("video") // check if necessary
+        video.classList.add("video-component") // check if necessary
         this.shadowRoot.appendChild(video);
         const source = document.createElement("source");
+        source.setAttribute("src", this.url)
         video.appendChild(source);
+        this.addVideoEventListeners(video);
         this.addStyle() //check if required
     }
 
+    addStyle() {
+        const styleTag = document.createElement("style");
+        styleTag.textContent = getStyle(this.width, this.heigth);
+        this.shadowRoot.appendChild(styleTag);
+    }
+
     addVideoEventListeners(video) {
-        video.addEventListener("play", ()=> {
-            video.classList.add("play");
+        video.addEventListener("onplay", ()=> {
+            this.enableWebMonetization();
+            // add the function which will be called when video is played
         })
-        video.addEventListener("pause", ()=> {
-            video.classList.add("pause")
+        video.addEventListener("onpause", ()=> {
+            this.disableWebMonatization();
+            // add the function which will be called when the video is paused 
         })
         // listen for current frame
-        video.addEventListener("listen", ()=> {
-            video.classList.remove("listen")
+        video.addEventListener("onratechange", ()=> {
+            console.log("You have increased or decreased the video speed you will be charged on the basis of how video viewed!")
         })
-    }
-
-    createSource()
-
-    // will update this later if required!!
-    // addSourceTag(source) {
-    //     [...this.text].forEach(letter=> {
-    //         let source = this.createSource(letter)
-
-    //     })
-    // }
-
-    disconnectedCallback() {
-        //called when element is removed from DOM.
-        console.log('disconnected!');
-    }
-
-    attributeChangedCallback(name, oldVal, newVal) {
-        // called when elment observed attribute change.
-        console.log(`Attribute: ${name} changed!`);
-    }
-
-    adoptedCallback() {
-        //custom element is moved to a new document. Only run into this use case when you have <iframe>
-        console.log('adopted!');
     }
 }
 
 
-window.customElements.define('web-monetized-video', WebMonetizedVideo)
+try {
+    customElements.define('web-monetized-video', WebMonetizedVideo)
+} catch(err) {
+    const h3 = document.createElement('h3');
+    h3.innerHTML = "This site uses webcomponents which don't work in all browsers! Try this site in a browser that supports them!";
+    document.body.appendChild(h3);
+}
