f843ed88c857f825d204b6277c9da9fb43b5586a
buckleader <jasmin28virdi@gmail.com>
2020-05-25 05:21:06 +0530

    delete files

--- src/components/web-monetized-video/index.js
+++
@@ -1,4 +0,0 @@
-import { WebMonetizedVideo } from './web-monetized-video';
-if (!window.customElements.get('web-monetized-video')) {
-    window.customElements.define('web-monetized-video', WebMonetizedVideo);
-}
--- src/demo/index.js
+++
@@ -1,7 +0,0 @@
-import '../main';
-
-const webMonetizationComponent = document.createElement('web-monetized-video');
-
-webMonetizationComponent.setAttribute("url", "https://www.html5rocks.com/en/tutorials/video/basics/Chrome_ImF.webm" )
-webMonetizationComponent.setAttribute("monetization-link", "$wallet.example.com/alice" )
-document.body.appendChild(webMonetizationComponent);
