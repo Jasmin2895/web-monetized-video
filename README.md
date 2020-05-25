b98db6c0393115a2c67f1dd03585d78e4340b579
buckleader <jasmin28virdi@gmail.com>
2020-05-25 05:42:47 +0530

    new file structure

---
+++ README.md
@@ -0,0 +1,16 @@
+# web-monetized-video
+It is a web component which has play and pay policy. If you watch half of the video you will pay for that no need to pay for the full video. :smile: :dancer:
+
+
+## Component:
+* [`<web-monetized-video>`] - a component that allow the user to pay for the amount of video watched.
+
+## How to use
+```<web-monetized-video width="300" heigth="200" url="video_url" monetization-link="payment_pointer"></web-monetized-video>```
+
+### Parameters
+* `url`- Link of the video. (Ex- `https://www.html5rocks.com/en/tutorials/video/basics/Chrome_ImF.webm`)
+* `monetization-link`- payment wallet of the user (Ex- `$wallet.example.com/alice`)
+## LICENSE
+
+MIT (c) 2020 Jasmin Virdi
---
+++ web-monetized-video.js
@@ -0,0 +1,96 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+    :host {
+    display: block;
+    font-family: sans-serif;
+    text-align: center;
+    }
+</style>
+<video id="video" controls="controls"  >
+    <source/>
+</video>
+`
+
+class WebMonetizedVideo extends HTMLElement{
+    constructor() {
+        super();
+        this.attachShadow({ 'mode': 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.$video = this.shadowRoot.getElementById("video");
+        this.$source = this.$video.childNodes[1]
+        this.setInitialFlags();
+    }
+
+    setInitialFlags() {
+        this.hasWebMonetizationEnabled= false;
+        this.onceWatchedFully = false;
+    }
+
+    connectedCallback() {
+        this.width = this.getAttribute("width");
+        this.heigth = this.getAttribute('heigth');
+        this.url = this.getAttribute('url');
+        this.getPaymentDetails = this.getAttribute("monetization-link")
+        this.setProperties();
+        this.isWebMonetized();
+    }
+
+    setProperties() {
+        this.$video.setAttribute("width", this.width);
+        this.$video.setAttribute("heigth", this.heigth);
+        this.$source.setAttribute("src", this.url);
+        this.addVideoEventListeners();
+    }
+    
+    isWebMonetized() {
+        if(!document.monetized) {
+            this.hasWebMonetizationEnabled = false;
+        } else {
+            this.hasWebMonetizationEnabled = true;
+        }
+    }
+
+    addVideoEventListeners() {
+        this.$video.addEventListener("play", ()=> {
+            this.enableWebMonetization();
+        })
+
+        this.$video.addEventListener("pause", ()=> {
+            this.disableWebMonatization();
+        })
+
+        this.$video.addEventListener("ended", () => {
+            this.disableWebMonatization();
+            this.onceWatchedFully = true; 
+        })
+    }
+
+    enableWebMonetization() {
+        if(!this.hasWebMonetizationEnabled && !this.onceWatchedFully) {
+            const monetizationTag = document.createElement('meta');
+            monetizationTag.name = "monetization";
+            monetizationTag.content = this.getPaymentDetails;
+            document.head.appendChild(monetizationTag);
+            this.hasWebMonetizationEnabled = true;
+        }
+    }
+
+    disableWebMonatization() {
+        this.hasWebMonetizationEnabled = false;
+        const removeMonetizationTag = document.querySelector('meta[name="monetization"]')
+        removeMonetizationTag.remove()
+    }
+
+    webMonetizationEventListeners() {
+        if(this.hasWebMonetizationEnabled){
+            // dispatch events when monetization process starts
+        }
+    }
+}
+
+if(!window.customElements.get("web-monetized-video")){
+    window.customElements.define('web-monetized-video', WebMonetizedVideo);
+}
+
+export default WebMonetizedVideo;
